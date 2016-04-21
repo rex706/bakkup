8520f637b9e2d64b11007f6e3d1be0a285587b1e
rex706 <apsinakis@me.com>
2016-04-21 16:15:58 -0400

    small fixes
    - display version
    - update readme
    - add icon file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Automatically backup game saves to a Google Drive folder to be accessed from any
 
 Make sure to be logged into your Google Drive account
 
-Files will be saved to "\Google Drive\Game Saves\\'Game Name'". Directory will be created on first use if not found. 
+Files will be saved to "\Google Drive\bakkup\". Directory will be created on first use if not found. 
 
 GUI can be skipped and backup/run game automatically by creating a shortcut and using the parameter "-Game Name" (same name of bakkup directory folder)
 
--- bakkup/Form1.Designer.cs
+++ bakkup/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -95,11 +96,21 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "v1.2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(250, 244);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
@@ -126,6 +137,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
